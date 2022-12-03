ccdb9e2d5d21ae4424ad1bb4e1467e35bc400fba
Zarkwolff <diegogp11@gmail.com>
2022-12-03 10:06:42 -0300

    Criando a conexão com o banco de dados

---
+++ desafioAdsMetodista/src/Persist/Conexao.java
@@ -0,0 +1,25 @@
+
+package Persist;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexao {
+    
+    //Criando a conexão com o banco
+    public Connection getConnection(){
+        try{
+            String servidor = "jdbc:mysql://localhost:3306/condominio";
+            String user = "root";
+            String pass = "P@lwe1r@$";
+            
+            return DriverManager.getConnection(servidor, user, pass);
+        }
+        catch(SQLException e){
+            
+            throw new RuntimeException(e);
+        }   
+    }
+}
